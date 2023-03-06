ed29b055b3cbeaa067afa5c4bb852a7f65d45cc4
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-06 14:36:03 +0700

    details component

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class DetailsComponent extends Component
 {
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.details-component');
+        $product = Product::where('slug', $this->slug)->first();
+        $rel_products = Product::where('category_id', $product->category_id)->inRandomOrder()->limit(4)->get();
+        $new_products = Product::latest()->take(4)->get();
+        return view('livewire.details-component', ['product' => $product, 'rel_products' => $rel_products, 'new_products' => $new_products]);
     }
 }
