4b6eba0af192e40c409692e8b3011a8bf8f9c617
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-15 13:39:56 +0700

    push all code

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -5,10 +5,20 @@ namespace App\Http\Livewire;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class ShopComponent extends Component
 {
     use WithPagination;
+
+    //untuk nyimpen pesanan ke Cart
+    public function store($product_id, $product_name, $product_price)
+    {
+        //1 = quantitiy
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+        return redirect()->route('shop.cart');
+    }
 
     public function render()
     {
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\DetailsComponent;
@@ -51,6 +52,7 @@ Route::middleware(['auth'])->group(function () {
 ////ADMIN ROUTES
 Route::middleware(['auth', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories', AdminCategoriesComponent::class)->name('admin.categories');
 });
 
 require __DIR__ . '/auth.php';
